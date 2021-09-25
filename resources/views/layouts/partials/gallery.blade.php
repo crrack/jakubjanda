25bd4bafebb012976de855f5fd0a3b1bcad20dd4
Ondřej Štěpán <o.stepan@outlook.com>
2021-09-25 16:26:03 +0200

    move images to admin

--- resources/views/layouts/partials/gallery.blade.php
+++ resources/views/layouts/partials/gallery.blade.php
@@ -1,14 +1,14 @@
 <section class="pt-12">
     <div class="gallery">
-        <img src="./img/gallery/1.png" alt="Obrázek galerie 1">
-        <img src="./img/gallery/2.png" alt="Obrázek galerie 2">
-        <img src="./img/gallery/3.png" alt="Obrázek galerie 3">
-        <img src="./img/gallery/4.png" alt="Obrázek galerie 4">
-        <img src="./img/gallery/5.png" alt="Obrázek galerie 5">
-        <img src="./img/gallery/6.png" alt="Obrázek galerie 6">
-        <img src="./img/gallery/7.png" alt="Obrázek galerie 7">
-        <img src="./img/gallery/8.png" alt="Obrázek galerie 8">
-        <img src="./img/gallery/9.png" alt="Obrázek galerie 9">
+        <img src="https://janda.webdo.cz/storage/files/content/images/3092510832515.png" alt="Obrázek galerie 1">
+        <img src="https://janda.webdo.cz/storage/files/content/images/4092565552541.png" alt="Obrázek galerie 2">
+        <img src="https://janda.webdo.cz/storage/files/content/images/5092524245551.jpg" alt="Obrázek galerie 3">
+        <img src="https://janda.webdo.cz/storage/files/content/images/6092581455521.png" alt="Obrázek galerie 4">
+        <img src="https://janda.webdo.cz/storage/files/content/images/7092597524515.png" alt="Obrázek galerie 5">
+        <img src="https://janda.webdo.cz/storage/files/content/images/8092562551425.jpg" alt="Obrázek galerie 6">
+        <img src="https://janda.webdo.cz/storage/files/content/images/9092532515452.png" alt="Obrázek galerie 7">
+        <img src="https://janda.webdo.cz/storage/files/content/images/10092563154255.jpg" alt="Obrázek galerie 8">
+        <img src="https://janda.webdo.cz/storage/files/content/images/11092557545215.png" alt="Obrázek galerie 9">
     </div>
 </section>
 <script type="text/javascript">
