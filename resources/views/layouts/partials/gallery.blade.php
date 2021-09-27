574eb0d5086978fc8a8eee3ce6022f3e5f50b0bc
Ondřej Štěpán <o.stepan@outlook.com>
2021-09-27 02:04:57 +0200

    add new photos to gallery

--- resources/views/layouts/partials/gallery.blade.php
+++ resources/views/layouts/partials/gallery.blade.php
@@ -9,6 +9,18 @@
         <img src="https://janda.webdo.cz/storage/files/content/images/9092532515452.png" alt="Obrázek galerie 7">
         <img src="https://janda.webdo.cz/storage/files/content/images/10092563154255.jpg" alt="Obrázek galerie 8">
         <img src="https://janda.webdo.cz/storage/files/content/images/11092557545215.png" alt="Obrázek galerie 9">
+        <img src="https://janda.webdo.cz/storage/files/content/images/52092720200072.jpg" alt="Obrázek galerie 10">
+        <img src="https://janda.webdo.cz/storage/files/content/images/53092768702002.jpg" alt="Obrázek galerie 11">
+        <img src="https://janda.webdo.cz/storage/files/content/images/54092770200702.jpg" alt="Obrázek galerie 12">
+        <img src="https://janda.webdo.cz/storage/files/content/images/55092783272000.jpg" alt="Obrázek galerie 13">
+        <img src="https://janda.webdo.cz/storage/files/content/images/56092743020270.jpg" alt="Obrázek galerie 14">
+        <img src="https://janda.webdo.cz/storage/files/content/images/57092772000722.jpg" alt="Obrázek galerie 15">
+        <img src="https://janda.webdo.cz/storage/files/content/images/58092746200720.jpg" alt="Obrázek galerie 16">
+        <img src="https://janda.webdo.cz/storage/files/content/images/59092794720002.jpg" alt="Obrázek galerie 17">
+        <img src="https://janda.webdo.cz/storage/files/content/images/60092788027200.jpg" alt="Obrázek galerie 18">
+        <img src="https://janda.webdo.cz/storage/files/content/images/61092762022700.jpg" alt="Obrázek galerie 19">
+        <img src="https://janda.webdo.cz/storage/files/content/images/62092772702002.jpg" alt="Obrázek galerie 20">
+        <img src="https://janda.webdo.cz/storage/files/content/images/63092742002702.jpg" alt="Obrázek galerie 21">
     </div>
 </section>
 <script type="text/javascript">
