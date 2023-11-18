07cbdfb8ebab377c37eb61da5b817a646d1c9716
Ondřej Štěpán <ondrej.stepan@websta.cz>
2023-11-18 16:12:51 +0100

    move gallery

--- resources/views/layouts/partials/gallery.blade.php
+++ resources/views/layouts/partials/gallery.blade.php
@@ -1,26 +1,27 @@
 <section class="pt-12 print-hide">
     <div class="gallery">
-        <img src="https://janda.webdo.cz/storage/files/content/images/3092510832515.png" alt="Obrázek galerie 1">
-        <img src="https://janda.webdo.cz/storage/files/content/images/4092565552541.png" alt="Obrázek galerie 2">
-        <img src="https://janda.webdo.cz/storage/files/content/images/5092524245551.jpg" alt="Obrázek galerie 3">
-        <img src="https://janda.webdo.cz/storage/files/content/images/6092581455521.png" alt="Obrázek galerie 4">
-        <img src="https://janda.webdo.cz/storage/files/content/images/7092597524515.png" alt="Obrázek galerie 5">
-        <img src="https://janda.webdo.cz/storage/files/content/images/8092562551425.jpg" alt="Obrázek galerie 6">
-        <img src="https://janda.webdo.cz/storage/files/content/images/9092532515452.png" alt="Obrázek galerie 7">
-        <img src="https://janda.webdo.cz/storage/files/content/images/10092563154255.jpg" alt="Obrázek galerie 8">
-        <img src="https://janda.webdo.cz/storage/files/content/images/11092557545215.png" alt="Obrázek galerie 9">
-        <img src="https://janda.webdo.cz/storage/files/content/images/52092720200072.jpg" alt="Obrázek galerie 10">
-        <img src="https://janda.webdo.cz/storage/files/content/images/53092768702002.jpg" alt="Obrázek galerie 11">
-        <img src="https://janda.webdo.cz/storage/files/content/images/54092770200702.jpg" alt="Obrázek galerie 12">
-        <img src="https://janda.webdo.cz/storage/files/content/images/55092783272000.jpg" alt="Obrázek galerie 13">
-        <img src="https://janda.webdo.cz/storage/files/content/images/56092743020270.jpg" alt="Obrázek galerie 14">
-        <img src="https://janda.webdo.cz/storage/files/content/images/57092772000722.jpg" alt="Obrázek galerie 15">
-        <img src="https://janda.webdo.cz/storage/files/content/images/58092746200720.jpg" alt="Obrázek galerie 16">
-        <img src="https://janda.webdo.cz/storage/files/content/images/59092794720002.jpg" alt="Obrázek galerie 17">
-        <img src="https://janda.webdo.cz/storage/files/content/images/60092788027200.jpg" alt="Obrázek galerie 18">
-        <img src="https://janda.webdo.cz/storage/files/content/images/61092762022700.jpg" alt="Obrázek galerie 19">
-        <img src="https://janda.webdo.cz/storage/files/content/images/62092772702002.jpg" alt="Obrázek galerie 20">
-        <img src="https://janda.webdo.cz/storage/files/content/images/63092742002702.jpg" alt="Obrázek galerie 21">
+        <img src="./img/slider/1.jpg" alt="Obrázek galerie 1">
+        <img src="./img/slider/2.png" alt="Obrázek galerie 2">
+        <img src="./img/slider/3.png" alt="Obrázek galerie 3">
+        <img src="./img/slider/4.jpg" alt="Obrázek galerie 4">
+        <img src="./img/slider/5.png" alt="Obrázek galerie 5">
+        <img src="./img/slider/6.png" alt="Obrázek galerie 6">
+        <img src="./img/slider/7.jpg" alt="Obrázek galerie 7">
+        <img src="./img/slider/8.png" alt="Obrázek galerie 8">
+        <img src="./img/slider/9.jpg" alt="Obrázek galerie 9">
+        <img src="./img/slider/10.png" alt="Obrázek galerie 10">
+        <img src="./img/slider/11.jpg" alt="Obrázek galerie 11">
+        <img src="./img/slider/12.jpg" alt="Obrázek galerie 12">
+        <img src="./img/slider/13.jpg" alt="Obrázek galerie 13">
+        <img src="./img/slider/14.jpg" alt="Obrázek galerie 14">
+        <img src="./img/slider/15.jpg" alt="Obrázek galerie 15">
+        <img src="./img/slider/16.jpg" alt="Obrázek galerie 16">
+        <img src="./img/slider/17.jpg" alt="Obrázek galerie 17">
+        <img src="./img/slider/18.jpg" alt="Obrázek galerie 18">
+        <img src="./img/slider/19.jpg" alt="Obrázek galerie 19">
+        <img src="./img/slider/20.jpg" alt="Obrázek galerie 20">
+        <img src="./img/slider/21.jpg" alt="Obrázek galerie 21">
+        <img src="./img/slider/22.jpg" alt="Obrázek galerie 22">
     </div>
 </section>
 <script type="text/javascript">
